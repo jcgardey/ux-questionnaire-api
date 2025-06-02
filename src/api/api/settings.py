00e00b1060192e1d2933bdb2343474e946ac4bf7
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2025-06-02 13:29:12 +0000

    fix cors config

--- src/api/api/settings.py
+++ src/api/api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*5a#_m3%r0y5q)lnb*771ypmj9h__y^(nni2!=dt^g0mhdp8ia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'ux-questionnaire-api.okd.lifia.info.unlp.edu.ar']
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173', 'http://ux-questionnaire.okd.lifia.info.unlp.edu.ar']
 
 
 # Application definition
